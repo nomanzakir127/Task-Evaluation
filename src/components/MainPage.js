ee41efd887c5cad27e059a0b08be8a14f469db7d
Noman Zakir <nomanzakir127@gmail.com>
2021-04-13 18:06:13 +0500

    extra code

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,13 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import {useNavigate} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import axios from 'axios'
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
